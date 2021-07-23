1d0735ccbb1d5b17304bbe6557e5a9aafb1e9cca
catan271 <catan271@gmail.com>
2021-07-23 10:19:23 +0700

    contact icon

--- main.js
+++ main.js
@@ -1,26 +1,11 @@
-let banner = document.getElementById("banner");
-let i = 1;
-let bannerInner = [
-    `<img src="banner/ny.jpg" alt="New york">
-        <div class="banner-text">
-            <p style="margin: 10px; font-size: 24px">New York</p>
-            <p style="margin-bottom: 47px; margin-top: 16px; font-size: 15px">The atmosphere in New York is lorem ipsum.</p>
-        </div>`,
-    `<img src="banner/chicago.jpg" alt="Chicago">
-        <div class="banner-text">
-            <p style="margin: 10px; font-size: 24px">Chicago</p>
-            <p style="margin-bottom: 47px; margin-top: 16px; font-size: 15px">Thank you, Chicago - A night we won't forget.</p>
-        </div>`,
-    `<img src="banner/la.jpg" alt="Los Angeles">
-        <div class="banner-text">
-            <p style="margin: 10px; font-size: 24px">Los Angeles</p>
-            <p style="margin-bottom: 47px; margin-top: 16px; font-size: 15px">We had the best time playing at Venice Beach!</p>
-        </div>`
-]
+const banner = [document.getElementById("banner-1"), document.getElementById("banner-2"), document.getElementById("banner-3")]
+let i = 0, j = 1
 
-const changeBanner = setInterval(()=> {
-    banner.innerHTML = bannerInner[i] ;
-    banner.style.animation = 'animation: appear linear 1s';
-    i++;
-    if (i == 3) i = 0;
+setInterval(()=> {
+    banner[i].style.display = 'none'
+    banner[j].style.display = 'block'
+    i++
+    j++
+    if (i === 3) i = 0
+    if (j === 3) j = 0
 }, 4000);
